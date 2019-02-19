d2e012df284c9e2f3b9cf668bce64d0b04129063
Aamer Nabi <aamerpaul@gmail.com>
2019-02-19 22:16:03 +0530

    nav graph implementation

--- app/src/main/java/com/aamernabi/moments/views/MainActivity.kt
+++ app/src/main/java/com/aamernabi/moments/views/MainActivity.kt
@@ -1,38 +1,15 @@
 package com.aamernabi.moments.views
 
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.aamernabi.moments.R
-import com.aamernabi.moments.utils.OnItemClickListener
-import com.aamernabi.moments.viewmodels.PhotosViewModel
-import com.aamernabi.moments.views.adapter.PhotoAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity(), OnItemClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        val adapter = PhotoAdapter(this)
-        recycler_view.adapter = adapter
-
-        attachObservers(adapter)
-    }
-
-    private fun attachObservers(adapter: PhotoAdapter) {
-        val viewModel = ViewModelProviders.of(this).get(PhotosViewModel::class.java)
-        viewModel.photos.observe(this, Observer {
-            it ?: return@Observer
-            adapter.submitList(it)
-        })
-    }
-
-    override fun onItemClick(index: Int) {
-
     }
 }
--- app/src/main/java/com/aamernabi/moments/views/fragment/PhotosFragment.kt
+++ app/src/main/java/com/aamernabi/moments/views/fragment/PhotosFragment.kt
@@ -1,6 +1,8 @@
 package com.aamernabi.moments.views.fragment
 
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +10,12 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.aamernabi.moments.R
+import com.aamernabi.moments.utils.OnItemClickListener
+import com.aamernabi.moments.viewmodels.PhotosViewModel
+import com.aamernabi.moments.views.adapter.PhotoAdapter
+import kotlinx.android.synthetic.main.fragment_photos.*
 
-class PhotosFragment : Fragment() {
+class PhotosFragment : Fragment(), OnItemClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +26,22 @@ class PhotosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = PhotoAdapter(this)
+        recycler_view.adapter = adapter
+
+        attachObservers(adapter)
+    }
+
+    private fun attachObservers(adapter: PhotoAdapter) {
+        val viewModel = ViewModelProviders.of(this).get(PhotosViewModel::class.java)
+        viewModel.photos.observe(this, Observer {
+            it ?: return@Observer
+            adapter.submitList(it)
+        })
+    }
+
+    override fun onItemClick(index: Int) {
+
     }
 }
