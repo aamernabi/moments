10a83b202d089624bd37aa5c3a4f047216457e6d
Aamer Nabi <aamerpaul@gmail.com>
2019-03-06 23:50:31 +0530

    input validator issues fixed

--- app/src/main/java/com/aamernabi/moments/utils/TextInputUtils.kt
+++ app/src/main/java/com/aamernabi/moments/utils/TextInputUtils.kt
@@ -2,10 +2,16 @@ package com.aamernabi.moments.utils
 
 import android.widget.EditText
 
-val notEmpty: (String) -> Boolean = { it.isNotEmpty() }
-val ofLength: (String, Int) -> Boolean = { str, length -> str.length == length  }
+val notEmpty: String.() -> Boolean = { isNotEmpty() }
+fun ofLength(length: Int): String.() -> Boolean = { this.length == length  }
 
-fun EditText.validate(block: (text: String) -> Boolean): Boolean {
+inline fun EditText.validate(block: String.() -> Boolean): Boolean {
     val text = text?.toString() ?: ""
     return block(text)
+}
+
+fun test(e: EditText) {
+    val isNotEmpty = e.validate(notEmpty)
+    val hasValidLen = e.validate(ofLength(6))
+    val hasDigits = e.validate { contains("\\d".toRegex()) }
 }
