3f5f11e48542083b9a5aadb78bab8d840896aaa5
Aamer Nabi <aamerpaul@gmail.com>
2019-03-28 20:24:24 +0530

    firebase analytics added

--- app/src/main/java/com/aamernabi/moments/views/adapter/FullScreenAdapter.kt
+++ app/src/main/java/com/aamernabi/moments/views/adapter/FullScreenAdapter.kt
@@ -1,6 +1,7 @@
 package com.aamernabi.moments.views.adapter
 
 import android.graphics.drawable.Drawable
+import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
@@ -15,6 +16,7 @@ import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class FullScreenAdapter(
     private val photos: List<Photo>
@@ -32,8 +34,20 @@ class FullScreenAdapter(
 
         progressBar.visibility = View.VISIBLE
 
+        val selectedPhoto = photos[position]
+        val analytics = FirebaseAnalytics.getInstance(imageView.context)
+        val bundle = Bundle().apply {
+            putString(FirebaseAnalytics.Param.ITEM_ID, selectedPhoto.id)
+            putInt("photo_width", selectedPhoto.width)
+            putInt("photo_height", selectedPhoto.height)
+            putString("photo_color", selectedPhoto.color)
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        }
+        analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+
+
         Glide.with(view)
-            .load(photos[position].urls.regular)
+            .load(selectedPhoto.urls.regular)
             .listener(object: RequestListener<Drawable> {
                 override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
                     progressBar.visibility = View.GONE
