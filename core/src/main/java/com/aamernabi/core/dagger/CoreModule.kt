ca19d508609d6c58f238cf04bc63f90fc5c1e406
Aamer Paul <aamerpaul@gmail.com>
2020-04-19 23:23:41 +0530

    Added - Dagger2 Integration in core module

---
+++ core/src/main/java/com/aamernabi/core/dagger/CoreModule.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 aamernabi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aamernabi.core.dagger
+
+import com.aamernabi.core.BuildConfig
+import com.aamernabi.core.utils.CONNECTION_TIMEOUT
+import com.aamernabi.core.utils.READ_TIMEOUT
+import com.aamernabi.core.utils.WRITE_TIMEOUT
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+class CoreModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        level = when (BuildConfig.DEBUG) {
+            true -> HttpLoggingInterceptor.Level.BODY
+            else -> HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .retryOnConnectionFailure(false)
+            .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(moshi: Moshi) = MoshiConverterFactory.create(moshi)
+}
